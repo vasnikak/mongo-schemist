25a22214e0174d3fd1e21c90a2430f855fd82fc3
comocomo <vasnikak@gmail.com>
2020-07-31 16:26:39 -0700

    First Commit

---
+++ src/MongoDBConnection.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace MongoSchemist;
+
+use MongoDB\Client as MongoDBClient;
+use MongoSchemist\Exception\MongoDBConnectionException;
+
+class MongoDBConnection {
+
+	/**
+	 * Constructor (singleton).
+	 */
+	private function __constuct() { }
+
+	/**
+	 * Creates and returns a client for a MongoDB instance.
+	 *
+	 * @param string $connString    MongoDB connection string
+	 * @param array  $uriOptions    Additional connection string options
+     * @param array  $driverOptions Driver-specific options
+	 *
+	 * @throws MongoDBConnectionException In case the connection could not be established
+	 *
+	 * @return MongoDB\Client The client for the MongoDB instance.
+	 */
+	public static function getClient($connString, $uriOptions = [], $driverOptions = []) {
+		try {
+			return new MongoDBClient($connString);
+		}
+		catch (\Exception $e) {
+			throw new MongoDBConnectionException($e->getMessage());
+		}
+	}
+
+	/**
+	 * Tests if the connection is established with the MongoDB instance.
+	 *
+	 * @param MongoDB\Client $mongoClient A MongoDB client instance
+	 *
+	 * @throws MongoDBConnectionException In case the connection could not be established
+	 */
+	public static function testConnection($mongoClient) {
+		try {
+			$mongoClient->listDatabases();
+		}
+		catch (\Exception $e) {
+			throw new MongoDBConnectionException($e->getMessage());
+		}
+	}
+
+}
+
+?>
